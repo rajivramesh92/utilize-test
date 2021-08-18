d103121174f718784dbe72eae92f56de065ec234
Rajeev <chaurasia.rajivchaurasia.rajiv@gmail.com>
2021-08-18 17:45:39 +0530

    added search

--- app/controllers/couriers_controller.rb
+++ app/controllers/couriers_controller.rb
@@ -1,13 +1,24 @@
 class CouriersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_order, only: :show
 
   def index
     @couriers = current_user.received_orders
   end
 
+  def search_order
+    @courier = Courier.find_by(order_number: params[:order_number])
+    if @courier.present?
+      redirect_to courier_path(@courier.id)
+    else
+      @couriers = current_user.received_orders
+      render 'index'
+    end
+  end
+
   def new
     @courier = current_user.couriers.new
-    @users = User.pluck(:full_name, :id)
+    @users = User.where.not(id: current_user.id).pluck(:full_name, :id)
   end
 
   def create
@@ -15,6 +26,7 @@ class CouriersController < ApplicationController
     if @courier.valid?
       redirect_to couriers_path
     else
+      @users = User.where.not(id: current_user.id).include(:address).pluck(:full_name, :id)
       render 'new'
     end
   end
@@ -23,5 +35,9 @@ class CouriersController < ApplicationController
 
   def courier_params
     params.require(:courier).permit(:weight, :cost, :receiver_id, :payment_mode, :service_type)
+  end
+
+  def set_order
+    @courier = Courier.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
-  resources :couriers
+  resources :couriers do 
+    collection do
+      post 'search_order'
+    end
+  end
 end

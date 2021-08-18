bad252586d6ab685156f74a2c4dee1ceb327b964
Rajeev <chaurasia.rajivchaurasia.rajiv@gmail.com>
2021-08-18 17:12:48 +0530

    added courier form

---
+++ app/controllers/couriers_controller.rb
@@ -0,0 +1,27 @@
+class CouriersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @couriers = current_user.couriers
+  end
+
+  def new
+    @courier = current_user.couriers.new
+    @users = User.pluck(:full_name, :id)
+  end
+
+  def create
+    @courier = current_user.couriers.create(courier_params)
+    if @courier.valid?
+      redirect_to couriers_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def courier_params
+    params.require(:courier).permit(:weight, :cost, :receiver_id, :payment_mode, :service_type)
+  end
+end
--- app/models/courier.rb
+++ app/models/courier.rb
@@ -3,7 +3,15 @@ class Courier < ApplicationRecord
   belongs_to :address
   belongs_to :receiver, class_name: 'User', foreign_key: 'receiver_id'
 
+  before_validation :save_details, :if => Proc.new {|model| model.new_record? }
+
   validates :weight, :status, :service_type, :payment_mode, :cost, presence: true
 
   validates :order_number, presence: true, uniqueness: true
+
+  def save_details
+    self.order_number = "ORDER-#{SecureRandom.hex(6)}"
+    self.status = 'Sent'
+    self.address = self.receiver.address
+  end
 end

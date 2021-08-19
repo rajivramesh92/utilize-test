e2267eff5664f4e2317348e64c7fd694e1f0fa73
Rajeev <chaurasia.rajivchaurasia.rajiv@gmail.com>
2021-08-19 11:34:08 +0530

    added number validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,6 @@ class Address < ApplicationRecord
   belongs_to :user
   has_many :couriers
   validates :full_address, :pin_code, :phone_number, presence: true
+  validates_length_of :phone_number, minimum: 10, maximum: 10
+  validates :phone_number, :numericality => true
 end
